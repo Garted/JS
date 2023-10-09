6634a8b714672dc0894103126b86e0433feb3bf3
garted <garted93@gmail.com>
2023-10-09 17:38:25 +0300

    sdfsd

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,22 @@
 "use strict";
 
-const answers = [];
-answers.push(prompt("What is yor name?", ""));
-answers.push(prompt("What is your second name?", ""));
-answers.push(+prompt("How old are you?", ""));
-console.log(answers);
+const numberOfFilms = +prompt("how many films you watched?", "");
 
-console.log(answers[1]);
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-const category = "toys";
-console.log(`url/${category}`); //interpolation
+const a = prompt("Last film what did you watch?", "");
+const b = +prompt("What rate will you give it?", "");
+
+const c = prompt("Last film what did you watch?", "");
+const d = +prompt("What rate will you give it?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
